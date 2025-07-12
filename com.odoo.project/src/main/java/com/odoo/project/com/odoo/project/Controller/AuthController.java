566686aa771fe2853270538d5acbe30792edade4
Dhiraj Chaudhary <dhirajchaudhary199@gmail.com>
2025-07-12 13:30:39 +0530

    Added Jwt Authentication..

---
+++ com.odoo.project/src/main/java/com/odoo/project/com/odoo/project/Controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.odoo.project.com.odoo.project.Controller;
+
+import com.yourpackage.dto.AuthRequest;
+import com.yourpackage.dto.AuthResponse;
+import com.yourpackage.dto.RegisterRequest;
+import com.yourpackage.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
+
---
+++ com.odoo.project/src/main/java/com/odoo/project/com/odoo/project/DTO/AuthRequest.java
@@ -0,0 +1,10 @@
+package com.odoo.project.com.odoo.project.DTO;
+
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+    private String email;
+    private String password;
+}
+
